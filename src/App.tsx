4168d44b98c57fcfb23b3ef582ceb41ff9740284
lyxlwz <1850115720@qq.com>
2022-12-17 21:57:59 +0800

    ✨ 第一部分实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+
+
+import { useEffect, useState } from 'react'
 import './App.less'
 
+import Dinosaur from './components/dinosaur/index.jsx'
 function App() {
-
+  const [showOne, setShowwOne] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setShowwOne(false)
+    }, 3200)
+  }, [])
   return (
     <div className="App">
-
+      {
+        showOne ? (
+          <Dinosaur ></Dinosaur>
+        ) : null
+      }
     </div>
   )
 }
---
+++ src/components/dinosaur/index.jsx
@@ -0,0 +1,18 @@
+import './index.less'
+
+
+const dinosaur = () => {
+  return (
+    <div className='run'>
+      <div className='dinosaur jellyBeans'>
+        <div className='dino'></div>
+        <div className='eye'></div>
+        <div className='mouth'></div>
+        <div className='ground'></div>
+        <div className='comets'></div>
+      </div>
+    </div>
+  )
+}
+
+export default dinosaur
