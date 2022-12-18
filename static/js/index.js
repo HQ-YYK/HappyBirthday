bc6de156e54d24b0e1ab7b45ef4c4032b32e94e8
lyxlwz <1850115720@qq.com>
2022-12-18 14:57:28 +0800

    gx

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,28 @@
 var percent = 0
 
-function boxshake(){
+function boxshake () {
   $('.top').addClass('shake')
   $('.bottom').addClass('shake')
 }
 
-function boxOpen(){
+function boxOpen () {
   $('.hand.left').addClass("handLeftAnimation")
   $('.top').removeClass('shake')
   $('.top').addClass("topOpenAnimation")
 }
 
-function TextChange(){
-  $(".ghostText").text("Trick or Treat !")
+function TextChange () {
+  $(".ghostText").text("Happy Birthday !")
 }
 
-var timer = setInterval(function(){
-  $(".bar").css("width",percent+"%")
-  percent+=1
-  if (percent>=101){
+var timer = setInterval(function () {
+  $(".bar").css("width", percent + "%")
+  percent += 1
+  if (percent >= 101) {
     $(".pageLoading").addClass("complete")
     clearInterval(timer)
-    setTimeout(boxshake,500)
-    setTimeout(boxOpen,3000)
-    setTimeout(TextChange,4000)
+    setTimeout(boxshake, 500)
+    setTimeout(boxOpen, 3000)
+    setTimeout(TextChange, 4000)
   }
-},30)
+}, 30)
