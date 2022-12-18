7398a6402c50845d30b1637e3338dee246f91bcf
lyxlwz <1850115720@qq.com>
2022-12-18 14:16:34 +0800

    gx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 import './App.less'
 
 import Dinosaur from './components/dinosaur/index.jsx'
+import Typewriter from './components/typewriter/index.jsx'
+
 function App() {
   const [showOne, setShowwOne] = useState(true)
   useEffect(() => {
@@ -12,12 +14,13 @@ function App() {
     }, 3200)
   }, [])
   return (
-    <div className="App">
-      {
+    <div className="App h-screen flex fixed w-screen justify-center items-center text-center">
+      {/* {
         showOne ? (
           <Dinosaur ></Dinosaur>
         ) : null
-      }
+      } */}
+      <Typewriter></Typewriter>
     </div>
   )
 }
